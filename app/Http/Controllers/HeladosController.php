028011e8606328c78f1df8fd1113a16b5bde043b
CondeTonny <tonyconde0@gmail.com>
2021-09-01 09:42:51 -0500

    controller

--- app/Http/Controllers/HeladosController.php
+++ app/Http/Controllers/HeladosController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class HeladosController extends Controller
 {
-    //
+    public function index(){
+        return view('helados.index');
+    }
+    public function create(){
+        return view('helados.create');
+    }
+    public function show($helados){
+        return view('helados.show',['helados'=> $helados]);
+    }
 }
--- app/Http/Controllers/VentaController.php
+++ app/Http/Controllers/VentaController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class VentaController extends Controller
 {
-    //
+    public function index(){
+        return view('venta.index');
+    }
+    public function create(){
+        return view('venta.create');
+    }
+    public function show($venta){
+        return view('venta.show',['venta'=> $venta]);
+    }
 }
